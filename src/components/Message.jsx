4f35861cc4ab58e4dea4a8e9abd2fe24d25fc186
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-01-09 15:38:30 +0100

    Finish design with messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="messageinfo">
+        <img
+          src="https://cdn.pixabay.com/photo/2017/09/14/11/23/girl-2748687_960_720.jpg"
+          alt="user avatar"
+        />
+        <span>date</span>
+      </div>
+      <div className="messagecontent">
+        <p>Hello message</p>
+        <img
+          src="https://cdn.pixabay.com/photo/2017/09/14/11/23/girl-2748687_960_720.jpg"
+          alt="user avatar"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
